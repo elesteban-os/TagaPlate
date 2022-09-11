19edbca1c710844b3739f9432871bce3839a3275
Andrés Martínez Vargas <49073466+vmartinezandres@users.noreply.github.com>
2022-09-11 17:29:47 -0600

    Analizador sintáctico implementado
    Arreglos en el analizador léxico

--- Lenguaje-TagaPlate/src/analizadorLexico.py
+++ Lenguaje-TagaPlate/src/analizadorLexico.py
@@ -4,142 +4,73 @@ import codecs
 import os
 import sys
 
-tokens = ['ID', 'COMMENT', 'PPROC', 'PROC', 'LPARENT', 'RPARENT', 'CALL', 'SEMMICOLOM', 'NEW', 'COMMA', 
-        'DATATYPE', 'VALUE', 'VALUES', 'ALTER', 'OP', 'ALTERB', 'MOVERIGHT', 'MOVELEFT', 'HAMMER', 'DIRECTION', 
-        'STOP', 'LOGICOP', 'ISTRUE', 'REPEAT', 'UNTIL', 'WHILE', 'CASEWHEN', 'THEN', 'ELSE', 'CASE', 'WHEN', 'PRINTVALUES']
+tokens = ['PPROC', 'ID', 'PROC', 'CALL', 'NEW', 'NUMDT', 'BOOLDT', 'VALUES', 'ALTERB', 'ALTER',
+        'MOVERIGHT', 'MOVELEFT', 'HAMMER', 'STOP', 'ISTRUE', 'REPEAT', 'BREAK', 'UNTIL', 'WHILE', 'CASEWHEN',
+        'THEN', 'ELSE', 'CASE', 'WHEN', 'PRINTVALUES', 'NUMVALUE', 'BOOLVALUE', 'TEXT', 'ADDOP','SUBOP',
+        'MULOP', 'DIVOP', 'NORTH', 'SOUTH', 'EAST', 'WEST', 'EEOP', 'NEOP', 'GETOP','LETOP',
+        'GTOP', 'LTOP', 'COMMENT', 'LPAR', 'RPAR', 'SEMMICOLOM', 'COMMA']
 
 
 def t_PPROC(t):
     r'@Principal'
     return t
-    
-def t_ID(t):
-    r'@[\w#]{3,10}'    
-    return t
+
+t_ID = r'@[\w#]{3,10}'
+t_PROC = r'Proc'
+t_CALL = r'CALL'
+t_NEW = r'New' 
+t_NUMDT = r'Num'
+t_BOOLDT = r'Bool'
+t_VALUES = r'Values'
+t_ALTERB = r'AlterB'
+t_ALTER = r'Alter'
+
+t_MOVERIGHT = r'MoveRight'
+t_MOVELEFT = r'MoveLeft'
+t_HAMMER = r'Hammer'
+t_STOP = r'Stop'
+t_ISTRUE = r'IsTrue'
+t_REPEAT = r'Repeat'
+t_BREAK = r'Break'
+t_UNTIL = r'Until'
+t_WHILE = r'While'
+t_CASEWHEN = r'CaseWhen'
+
+t_THEN = r'Then'
+t_ELSE = r'Else'
+t_CASE = r'Case'
+t_WHEN = r'When'
+t_PRINTVALUES = r'PrintValues'
+t_NUMVALUE = r'\d+'
+t_BOOLVALUE = r'True|False'
+t_TEXT = r'".+"'
+t_ADDOP = r'ADD'
+t_SUBOP = r'SUB'
+
+t_MULOP = r'MUL'
+t_DIVOP = r'DIV'
+t_NORTH = r'N'
+t_SOUTH = r'S'
+t_EAST = r'E'
+t_WEST = r'O'
+t_EEOP = r'=='
+t_NEOP = r'<>'
+t_GETOP = r'>='
+t_LETOP = r'<='
+
+t_GTOP = r'>'
+t_LTOP = r'<'
+t_LPAR = r'\('
+t_RPAR = r'\)'
+t_SEMMICOLOM = r';'
+t_COMMA = r','
 
 def t_COMMENT(t):
     r'--.*'
-    return t
-
-def t_PROC(t):
-    r'Proc'
-    return t
-
-def t_LPARENT(t):
-    r'\('
-    return t
-
-def t_RPARENT(t):
-    r'\)'
-    return t
-
-def t_CALL(t):
-    r'CALL'
-    return t
-
-def t_SEMMICOLOM(t):
-    r';'
-    return t
-
-def t_NEW(t):
-    r'New'
-    return t
-
-def t_COMMA(t):
-    r','
-    return t
-
-def t_DATATYPE(t):
-    r'(Bool|Num|String)'
-    return t
-
-def t_VALUE(t):
-    r'(True|False|\d+|".+")'
-    return t
-
-def t_VALUES(t):
-    r'Values'
-    return t
-
-def t_ALTERB(t):
-    r'AlterB'
-    return t
-
-def t_ALTER(t):
-    r'Alter'
-    return t
-
-def t_OP(t):
-    r'(ADD|SUB|MUL|DIV)'
-    return t
-   
-def t_MOVERIGHT(t):
-    r'MoveRight'
-    return t
-    
-def t_MOVELEFT(t):
-    r'MoveLeft'
-    return t
-    
-def t_HAMMER(t):
-    r'Hammer'
-    return t
-   
-def t_STOP(t):
-    r'Stop'
-    return t
-    
-def t_LOGICOP(t):
-    r'(>|<|==|<>|<=|>=)'
-    return t
-    
-def t_ISTRUE(t):
-    r'IsTrue'
-    return t
-    
-def t_REPEAT(t):
-    r'Repeat'
-    return t
-    
-def t_UNTIL(t):
-    r'Until'
-    return t
-    
-def t_WHILE(t):
-    r'While'
-    return t
-    
-def t_CASEWHEN(t):
-    r'CaseWhen'
-    return t
-    
-def t_THEN(t):
-    r'Then'
-    return t
-    
-def t_ELSE(t):
-    r'Else'
-    return t
-    
-def t_CASE(t):
-    r'Case'
-    return t
-    
-def t_WHEN(t):
-    r'When'
-    return t
-    
-def t_PRINTVALUES(t):
-    r'PrintValues'
-    return t
-
-def t_DIRECTION(t):
-    r'(N|S|E|O)'
-    return t
-
-def t_error(t):
-    print("Caracter ilegal '%s'" %t.value[0])
-    t.lexer.skip(1)
+    if (t.lexer.lineno == 1):
+        return t
+    else:
+        pass
 
 def t_newline(t):
     r'\n+'
@@ -147,20 +78,11 @@ def t_newline(t):
 
 def t_SPACE(t):
     r'\s'
-    pass
+    pass   
 
-directorio = '/Users/vmartinezandres/Desktop/Lenguaje-TagaPlate/'
-archivo = 'prueba.tagaplate' #buscarFicheros(directorio)
-test = directorio + archivo
-fp = codecs.open(test, "r", "utf-8")
-cadena = fp.read()
-fp.close()
+def t_error(t):
+    print("Caracter ilegal '%s'" %t.value[0])
+    t.lexer.skip(1)
 
 analizador = lex.lex()
 
-analizador.input(cadena)
-
-while True:
-    tok = analizador.token()
-    if not tok : break
-    print(tok)
